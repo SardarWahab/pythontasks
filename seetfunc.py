98316dd4b54929002255424e8af4e34dabe1dd4f
SardarWahab <wahabsardar130@gmail.com>
2024-08-07 16:33:06 +0500

    mycom

---
+++ seetfunc.py
@@ -0,0 +1,7 @@
+s1 = {'Ahmad', 'Raza','Maaz','wahab'}
+s2 = { 'kashir', 'Ali','Usman','wahab'}
+s = s1.union(s2)
+# s= s1.intersection(s2)
+# s= s1.difference(s2)
+# s = s1.symmetric_difference(s2)
+print(s)
