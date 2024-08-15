3d7c2b4c1b4989e4e70ae0e534340218a1460e85
SardarWahab <wahabsardar130@gmail.com>
2024-08-15 17:12:19 +0500

    mycom

--- fln.py
+++ fln.py
@@ -1,17 +1,8 @@
-# li = ['usman', 'zeshan','bazar','wahab','tayyeb']
-# for n in li:
-#     if n == 'wahab':
-#        break
-       
-       
-# print(li)
-
 li = ['usman', 'zeshan', 'bazar', 'wahab', 'tayyeb']
-
-for name in li:  # Use 'name' to iterate over the list 'li'
-    if name == 'wahab':  # Check if the current name is 'wahab'
-        break  # Exit the loop if the name is 'wahab'
-    print(name)  # Print the name if it isn't 'wahab'
+for name in li:  
+    if name == 'wahab':  
+        break  
+    print(name)  
 
 
 
