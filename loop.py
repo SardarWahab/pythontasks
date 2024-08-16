7ee8d2a0714993dd8856113bb502633719e06eeb
SardarWahab <wahabsardar130@gmail.com>
2024-08-16 16:39:47 +0500

    mycom

--- loop.py
+++ loop.py
@@ -7,11 +7,12 @@
 para = """To improve communication within our team, please reach out to the following individuals for specific inquiries: For project updates, contact Emily at emily.project@company.com; for financial matters, email Mark at mark.finance@company.com; if you need IT support, get in touch with Sarah at sarah.tech@company.com; for HR-related questions, email Hannah at hannah.hr@company.com; and for any general inquiries, you can reach out to John at john.info@company.com."""
 
 para_list = para.split(" ")
+li = []
 # print(para_list)
 for x in para_list:
- if '.com' in x:
-    print(x)  # prints the email addresses  
-
-
-
-
+     if '.com' in x:
+        print(x)  # prints the email addresses  
+        li.append(x)
+print(li)  
+print(len(li))
+ 
