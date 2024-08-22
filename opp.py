37bc763d725de1a07b06d3e2bf84f42fe6f03efa
SardarWahab <wahabsardar130@gmail.com>
2024-08-22 22:14:31 +0500

    mycom

---
+++ opp.py
@@ -0,0 +1,11 @@
+class wahab:
+    def __init__(self,name,roll,address):
+        self.name = name
+        self.roll = roll
+        self.address = address
+
+
+
+record = wahab('Wahab',40,'Mandhole')
+print(record.name,record.roll,record.address)
+        
