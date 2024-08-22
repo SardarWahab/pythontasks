fd892f7bd73bbd1c4ed08de4ce8142195a8d432a
SardarWahab <wahabsardar130@gmail.com>
2024-08-22 15:49:35 +0500

    mycom

---
+++ 22_1.py
@@ -0,0 +1,13 @@
+# def square(n):
+#     return n**2
+
+# b= int(input("Please Enter a number:"))
+# a = square(b)
+# print(f"The Square of {b} is  {a}")
+
+
+def ad(*num):
+    return sum(num)
+   
+a = ad(11,12)
+print(a)  # Output: 23
